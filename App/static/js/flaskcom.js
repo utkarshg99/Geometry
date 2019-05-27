b3d93252a5b53372775ca41cc12f5351f8145bed
Utkarsh Gupta <utkarshg99@gmail.com>
2019-05-27 17:08:38 +0530

    initiate django app\

--- App/static/js/flaskcom.js
+++
@@ -1,9 +0,0 @@
-// Send the same request
-fetch('/hello')
-    .then(function (response) {
-        return response.json(); // But parse it as JSON this time
-    })
-    .then(function (json) {
-        console.log('GET response as JSON:');
-        console.log(json); // Here’s our JSON object
-    })
