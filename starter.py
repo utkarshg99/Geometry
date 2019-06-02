e9408989435ed3b0ca4ce6d0da322fec251ad404
Utkarsh Gupta <utkarshg99@gmail.com>
2019-06-02 21:16:11 +0530

    Speech Recognition Added : Issue #8

--- starter.py
+++ starter.py
@@ -1,9 +1,9 @@
 import os
 os.system("npm i node-watch")
 if(os.name=='nt'):
-    os.system('start "" python ./Backend/runit.py')
-    os.system('start "" node ./Middleware/strserve.js')
-    os.system('start "" ./Frontend/UI.html')
+    os.system('start "Backend : Computational Engine" python ./Backend/runit.py')
+    os.system('start "Middleware : Transfer Server" node ./Middleware/strserve.js')
+    os.system('start "Frontend : User Interface" ./Frontend/UI.html')
 if(os.name=='posix'):
     os.system('python ./Backend/runit.py &')
     os.system('node ./Middleware/strserve.js &')
